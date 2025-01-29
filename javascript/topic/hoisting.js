f63f6ecf8cc8514dad15af210b6d9595b7f4cd70
Niyas10 <niyazmuhammad688@gmail.com>
2025-01-29 21:51:49 +0530

    first commit

---
+++ javascript/topic/hoisting.js
@@ -0,0 +1,31 @@
+
+
+// undefined
+
+// console.log(a);     
+// var a = 10
+
+
+// ........................................................................................................................
+
+
+// reference error 
+
+// console.log(b);
+// let b = 20
+
+
+// ........................................................................................................................
+
+
+// this code run perfectly
+
+
+// Task()
+
+// function Task() {
+//     console.log("hello")
+// }
+
+// var varbile function declire cheyth annel type error beacuse verible hoisted an 
+// but let const annel reference error avum
